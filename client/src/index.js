516d8e9542a7e8ef93701364bf7abc743a051aef
ndurbin <nick.durbin@gmail.com>
2019-10-24 03:31:13 -0400

    Did some boiler plate redux in the index.js component.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import logger from 'redux-logger'
+import { Provider } from 'react-redux'
+import { reducer } from './reducers'
+import './index.css'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducer, applyMiddleware(thunk, logger))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
