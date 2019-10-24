bc05fa52e132be818a0d8191b9209269d4b51ba3
ndurbin <nick.durbin@gmail.com>
2019-10-24 04:03:03 -0400

    Setup my actions and my reducer with initial state.

---
+++ client/src/actions/books.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+export const GET_BOOKS_REQUEST = 'GET_BOOKS_REQUEST'
+export const GET_BOOKS_SUCCESS = 'GET_BOOKS_SUCCESS'
+export const GET_BOOKS_ERROR = 'GET_BOOKS_ERROR'
+
+export const getMarvelComics = () => dispatch => {
+  dispatch({ type: GET_BOOKS_REQUEST })
+  axios
+    .get(`https://api.itbook.store/1.0/search/${category}/${page}`)
+    .then(res =>
+      dispatch({ type: GET_BOOKS_SUCCESS, payload: res.data })
+    )
+    .catch(err => dispatch({ type: GET_BOOKS_ERROR, payload: err }))
+}
